064ba6041de5ccd6b6fdd5434f4a0330861cb345
Aaron Helton <mariusagricola@gmail.com>
2021-04-16 15:19:36 -0400

    set event handling and update requirements

--- service.py
+++ service.py
@@ -1,11 +1,19 @@
-from datetime import datetime, timezone
 from gdoc_api.scripts import gdoc_dlx
 import sys
 
 def handler(event, context):
     # This is necessary because the python-lambda commands get passed to the gdoc_dlx.run function
     sys.argv = [sys.argv[0]]
-    print(event)
+    
+    try:
+        duty_station = event["duty_station"]
+    except:
+        duty_station = "NY"
+    try:
+        date = event["date"]
+    except:
+        date = "2021-03-01"
+
     try:
         gdoc_dlx.run(station="NY", date="2021-03-01")
     except:
