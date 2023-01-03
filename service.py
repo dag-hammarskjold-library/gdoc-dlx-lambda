83f714a435394cbe5b2ae8690170b0fd068f3b73
J. Bukhari <jb@Js-MacBook-Air.local>
2023-01-03 14:57:26 -0500

    use "days_ago" parameter

--- service.py
+++ service.py
@@ -2,26 +2,15 @@ from gdoc_api.scripts import gdoc_dlx
 import sys, datetime
 
 def handler(event, context):
-    # This is necessary because the python-lambda commands get passed to the gdoc_dlx.run function
-    sys.argv = [sys.argv[0]]
-
     print(f"Processing {event}")
     
-    try:
-        duty_station = event["duty_station"]
-    except:
-        duty_station = "NY"
-    try:
-        date = event["date"]
-    except:
-        today = datetime.date.today()
-        yesterday = today - datetime.timedelta(days=1)
-        date = yesterday
-
-    try:
-        gdoc_dlx.run(station=duty_station, date=date, recursive=True)
-    except:
-        raise
+    for param in ['duty_station', 'days_ago']:    
+        if event.get(param) is None:
+            raise Exception(f'Event parameter "{param}" is required')
+    
+    today = datetime.date.today()
+    date = today - datetime.timedelta(days=event['days_ago'])
+    gdoc_dlx.run(station=event['duty_station'], date=date, recursive=True)
 
     return {
         'status_code': 200
